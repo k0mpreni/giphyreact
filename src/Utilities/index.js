cea953d4fe3780548149ef81b90efcd2b0d72955
Alex Labonne <alex.labonne@ekino.com>
2018-03-23 15:54:47 +0100

    app: change fetch for https

--- src/Utilities/index.js
+++ src/Utilities/index.js
@@ -1,2 +1,2 @@
 export const makeFetchGifsUrl = value =>
-  `http://api.giphy.com/v1/gifs/search?q=${value}&api_key=vw4usscjAkDQiPbUvnGdRJfpEUWqZsuY`;
+  `https://api.giphy.com/v1/gifs/search?q=${value}&api_key=vw4usscjAkDQiPbUvnGdRJfpEUWqZsuY`;
